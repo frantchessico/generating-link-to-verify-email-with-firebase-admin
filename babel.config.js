d2547372d66cd25af77701b5cc04442437887961
DaltonHarvey <daltonharvey730@gmail.com>
2020-07-16 08:32:40 +0200

    adicionado configuracoes do babel

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current'
+          }
+        }
+      ],
+      '@babel/preset-typescript'
+    ],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ['module-resolver', {
+        alias: {
+          '@config': './src/config',
+          '@models': './src/models',
+          '@controllers': './src/controllers',
+        }
+      }]
+    ],
+    ignore: [
+      '**/*.spec.ts'
+    ]
+  }
